ebd52b1fda66bb3572e7f6ec5866c649e1654692
JuanPabloLunaSanchez <113323847+PabloLunaSanchez@users.noreply.github.com>
2024-06-21 14:30:43 -0600

    Update constantes.dart

--- lib/componets.dart/constantes.dart
+++ lib/componets.dart/constantes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const String google_api_key = "AIzaSyDC4gf97vPCcFLeNrcofvmNOqY3KUDjzZc";
+const String google_api_key = "";
 const Color primaryColor = Color(0xff7b61ff);
 const double defaultPadding = 16.0;
